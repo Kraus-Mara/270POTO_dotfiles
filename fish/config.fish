74ba44e20aed82d501b8c9eea3ec33284a95a67b
Kraus-Mara <krausantoi@cy-tech.fr>
2025-09-09 09:56:11 +0200

    dotfiles added

---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+starship init fish | source
+set fish_greeting
+if status is-interactive
+
+end
+
+function starp
+    if test (count $argv) -eq 0
+        echo "Usage: starp <profile_name>"
+        return 1
+    end
+
+    set profile $argv[1]
+    set config_path ~/.config/starship/$profile.toml
+    set target_path ~/.config/starship.toml
+
+    if test -f $config_path
+        ln -sf $config_path $target_path
+        echo "Starship profile switched to '$profile'"
+        exec fish
+    else
+        echo "Profile '$profile' does not exist!"
+        return 1
+    end
+end
+
